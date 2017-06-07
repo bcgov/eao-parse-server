9125345979340df7aeff64350fda0d5dc2a2aa84
Mark Lise <mark.lise@gmail.com>
2017-06-06 18:42:22 -0700

    Adding parse-dashboard, reworking some code, code cleanup and setting up environment variables.

--- index.js
+++ index.js
@@ -1,25 +1,53 @@
-// Example express application adding the parse-server module to expose Parse
-// compatible API routes.
+var express               = require('express');
+var ParseServer           = require('parse-server').ParseServer;
+var ParseDashboard        = require('parse-dashboard');
+var path                  = require('path');
 
-var express = require('express');
-var ParseServer = require('parse-server').ParseServer;
-var path = require('path');
+var port                  = process.env.PORT || 1337;
+var databaseUri           = process.env.DATABASE_URI || process.env.MONGODB_URI || 'mongodb://localhost:27017/parse';
+var mountPath             = process.env.PARSE_MOUNT || '/parse';
+var isProduction          = process.env.PRODUCTION_ENV || false;
+var serverURL             = process.env.SERVER_URL || 'http://localhost:1337/parse';
+var appId                 = process.env.APP_ID || 'myAppId';
+var masterKey             = process.env.MASTER_KEY || 'abc123';
+var appName               = process.env.PARSE_APP_NAME || 'parse-server';
+var javascriptKey         = process.env.JAVASCRIPT_KEY;
+var userJsonString        = process.env.USERS_JSON || '[{"user": "test", "pass": "test"}]';
 
-var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
+var userArray             = JSON.parse(userJsonString);
+var allowInsecureHttp     = true;
+var useEncryptedPassword  = false;
 
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  databaseURI: databaseUri,
+  appId: appId,
+  masterKey: masterKey,
+  serverURL: serverURL,
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
 });
+
+// setup the dashboard
+var dashboard = new ParseDashboard({
+  "apps": [
+    {
+      "serverURL": serverURL,
+      "appId": appId,
+      "masterKey": masterKey,
+      "appName": appName,
+      "production": isProduction,
+      "javascriptKey": javascriptKey
+    }
+  ],
+  "users": userArray,
+  "useEncryptedPassword": useEncryptedPassword
+}, allowInsecureHttp);
+
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
@@ -30,21 +58,25 @@ var app = express();
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
 // Serve the Parse API on the /parse URL prefix
-var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
+// Dashboard
+app.use("/dashboard", dashboard);
 
 // Parse Server plays nicely with the rest of your web routes
 app.get('/', function(req, res) {
   res.status(200).send('Your installation of parse-server is complete!');
 });
 
+
+
 // There will be a test page available on the /test path of your server url
 // Remove this before launching your app
-app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
-});
+if (!isProduction) {
+  app.get('/test', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public/test.html'));
+  });
+}
 
-var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
 httpServer.listen(port, function() {
     console.log('parse-server-example running on port ' + port + '.');
